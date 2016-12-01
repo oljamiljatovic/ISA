8b22980aec8fe319be1a6310b7fe89add893ee5f
unknown <olja.miljatovic@yahoo.com>
2016-12-01 11:14:49 +0100

    Izbrisan stari projekat

--- IsaProjekat/src/main/resources/dataOlja.sql
+++
@@ -1,2 +0,0 @@
-insert into city(country, name, state, map) values ('Olja', 'Oljino', 'Oljka', '-27.470933, 153.023502')
-insert into hotel(city_id, name, address, zip) values (1, 'Oljin hotel', 'AdresaOlja', '4001')
