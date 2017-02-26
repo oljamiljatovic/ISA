64ebd8b9abe5f9a660eb8a91dd9d28ae55712875
DesankaTodic <desankatodic@yahoo.com>
2017-02-26 16:32:43 +0100

    Omoguceno je da racun moze kreirati samoonaj konobar koji je primio narudzbinu, a smjena mu jos uvijek traje.

--- jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/BillController.java
+++ jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/BillController.java
@@ -2,7 +2,6 @@ package rs.ac.uns.ftn.informatika.jpa.controller;
 
 import java.util.ArrayList;
 
-import rs.ac.uns.ftn.informatika.jpa.domain.Order;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +16,11 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import rs.ac.uns.ftn.informatika.jpa.domain.Bill;
+import rs.ac.uns.ftn.informatika.jpa.domain.Order;
 import rs.ac.uns.ftn.informatika.jpa.domain.User;
 import rs.ac.uns.ftn.informatika.jpa.service.BillService;
 import rs.ac.uns.ftn.informatika.jpa.service.OrderService;
+import rs.ac.uns.ftn.informatika.jpa.service.WorkScheduleService;
 
 @Controller 
 @RequestMapping("/billController")
@@ -28,6 +29,8 @@ public class BillController {
 	private BillService billService;
 	@Autowired
 	private OrderService orderService;
+	@Autowired
+	private WorkScheduleService workScheduleService;
 	
 	@RequestMapping(
 			value = "/getBills",
@@ -64,14 +67,26 @@ public class BillController {
 		ServletRequestAttributes attr = (ServletRequestAttributes) 
 			    RequestContextHolder.currentRequestAttributes();
 		HttpSession session= attr.getRequest().getSession(true);
-		User u = (User) session.getAttribute("korisnik");
-		bill.setWaiter_id(u.getId());
-		Bill addedBill = billService.addNew(bill);
+		User u = (User) session.getAttribute("korisnik");		
 		Long orderId = bill.getOrder_id();
 		Order orderForClose = this.orderService.findById(orderId);
+		//////////////////////////
 		orderForClose.setBarman_state("kraj");
 		orderForClose.setCook_state("kraj");
 		Order changedOrder = orderService.update(orderForClose, orderId);
+		/////////////////////////
+		Long waiterIdFromOrder = orderForClose.getWaiter_id();
+		Long waiterIdFromSession = u.getId();
+		///ako onaj ko kreira racun i onaj na kome je porudzbina nisu isti
+		//provjeri da li je se onom sa porudzbine zavrsila smjena
+		//ako jest onda provjeri koje je duze opsluzivao
+		if(!waiterIdFromOrder.equals(waiterIdFromSession)){
+			//ArrayList<WorkSchedule> 
+			bill.setWaiter_id(waiterIdFromSession);
+		}else{
+			bill.setWaiter_id(waiterIdFromSession);
+		}
+		Bill addedBill = billService.addNew(bill);
 		return new ResponseEntity<Bill>(addedBill, HttpStatus.OK);
 	}
 	
